1ae05695b5c03f1dfbb2a9ab81c4ae39549b8f4a
Alexander Mantzoukas <ama@proactive.dk>
2019-12-14 23:08:00 +0100

    Add the basic provided greedy policy

---
+++ Selenium.Algorithms/RandomExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Selenium.Algorithms
+{
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
+
+    public static class RandomExtensions
+    {
+        public static RandomState Save(this Random random)
+        {
+            var binaryFormatter = new BinaryFormatter();
+            using (var temp = new MemoryStream())
+            {
+                binaryFormatter.Serialize(temp, random);
+                return new RandomState(temp.ToArray());
+            }
+        }
+
+        public static Random Restore(this RandomState state)
+        {
+            var binaryFormatter = new BinaryFormatter();
+            using (var temp = new MemoryStream(state.State))
+            {
+                return (Random)binaryFormatter.Deserialize(temp);
+            }
+        }
+    }
+
+    public struct RandomState
+    {
+        public readonly byte[] State;
+        public RandomState(byte[] state)
+        {
+            State = state;
+        }
+    }
+}
---
+++ Selenium.Algorithms/SeleniumQLearningStepPolicy.cs
@@ -0,0 +1,51 @@
+﻿namespace Selenium.Algorithms
+{
+    using OpenQA.Selenium;
+    using Selenium.Algorithms.ReinforcementLearning;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class SeleniumQLearningStepPolicy : Policy<IReadOnlyCollection<IWebElement>>
+    {
+        private readonly Random random;
+
+        public SeleniumQLearningStepPolicy(Random random)
+        {
+            this.random = random;
+        }
+
+        public override async Task<AgentAction<IReadOnlyCollection<IWebElement>>> GetNextAction(Environment<IReadOnlyCollection<IWebElement>> environment, State<IReadOnlyCollection<IWebElement>> state)
+        {
+            // TODO: typing: when element is an input, type instead of click (remember it has to be able to be traced from the goal/reward)
+
+            // Intermediate states (e.g. loading): when you have a state with no elements, create a wait-for-time-action
+            if (state.Data.Count == 0)
+            {
+                return new WaitAction(random.Next(100, 500));
+            }
+
+            // TODO: analyse the existing Q-matrix data and find the best action
+            // TODO: exploration mode?
+            var actions = await environment.GetPossibleActions(state);
+            var stateAndActionPairs = actions
+                .Select(x =>
+                {
+                    var pair = new StateAndActionPairWithResultState<IReadOnlyCollection<IWebElement>>(state, x);
+                    return QualityMatrix.ContainsKey(pair)
+                        ? (Action: x, Score: QualityMatrix[pair])
+                        : (Action: x, Score: 0D);
+                })
+                .OrderByDescending(x => x.Score)
+                .ToList();
+            
+            // Take all the items that compete for the maximum value
+            var maxStateAndActionPairs = stateAndActionPairs
+                .Where(x => x.Score == stateAndActionPairs[0].Score)
+                .ToList();
+
+            return maxStateAndActionPairs.ElementAt(random.Next(0, maxStateAndActionPairs.Count)).Action;
+        }
+    }
+}
