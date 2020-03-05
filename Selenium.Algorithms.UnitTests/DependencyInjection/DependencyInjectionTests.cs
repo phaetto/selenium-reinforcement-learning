f09412f665ca43e5c6109eb424ff6a21a8681e03
Alexander Mantzoukas <ama@proactive.dk>
2020-03-05 16:13:25 +0100

    Remove unused classes

--- Selenium.Algorithms.UnitTests/DependencyInjection/DependencyInjectionTests.cs
+++ Selenium.Algorithms.UnitTests/DependencyInjection/DependencyInjectionTests.cs
@@ -3,10 +3,8 @@
     using Microsoft.Extensions.DependencyInjection;
     using Moq;
     using OpenQA.Selenium;
-    using OpenQA.Selenium.Remote;
     using Selenium.Algorithms.ReinforcementLearning;
     using System.Collections.Generic;
-    using System.Threading.Tasks;
     using Xunit;
 
     /// <summary>
@@ -38,59 +36,6 @@
             var serviceProvider = services.BuildServiceProvider();
 
             serviceProvider.GetRequiredService<IEnvironment<IReadOnlyCollection<ElementData>>>();
-        }
-
-        class TrainGoal : ITrainGoal<int>
-        {
-            public int TimesReachedGoal { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-
-            public Task<bool> HasReachedAGoalCondition(IState<int> state, IAgentAction<int> action)
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public Task<double> RewardFunction(IState<int> stateFrom, IAgentAction<int> action)
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
-        class TestEnvironment : IEnvironment<int>
-        {
-            public Task<IState<int>> GetCurrentState()
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public Task<IState<int>> GetInitialState()
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public Task<IEnumerable<IAgentAction<int>>> GetPossibleActions(IState<int> state)
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public Task<bool> IsIntermediateState(IState<int> state)
-            {
-                throw new System.NotImplementedException();
-            }
-
-            public Task WaitForPostActionIntermediateStabilization()
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
-        class TestPolicy : IPolicy<int>
-        {
-            public IDictionary<StateAndActionPair<int>, double> QualityMatrix { get; } = new Dictionary<StateAndActionPair<int>, double>();
-
-            public Task<IAgentAction<int>> GetNextAction(IEnvironment<int> environment, IState<int> state)
-            {
-                throw new System.NotImplementedException();
-            }
         }
     }
 }
